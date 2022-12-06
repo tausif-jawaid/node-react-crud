79aca47beb9635fcfdf5ba718bd43c0f6987bee5
Mohammad Tausif Jawaid <mohammadtausif.jawai@hcl.com>
2022-12-06 15:37:04 +0530

    remove comment in server.js file

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const cors = require('cors')
 
 // express app
-// server.js file changes in Backend branc
 const app = express();
 app.use(express.json())
 app.use(cors())
